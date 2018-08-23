5afeda7bd456a004364a6fc553f206ba2125181d
Brady Mills <42005304+BradyMills02@users.noreply.github.com>
2018-08-23 08:43:53 -0600

    Button fix

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-var myArray = ["What do you think I'm going to say?", "Brady is never wrong", "Don't be stupid"];
-var create = document.createElement('div');
-
-function myBtn(e) {
-	create.setAttribute('id', 'response');
-	create.innerHTML = myArray;
-	document.body.appendChild(create);
+var myArray = ["What do you think I'm going to say?", 
+"Brady is never wrong", 
+"Don't be stupid"];
+function btn() {
+	document.getElementById("response").innerHTML = myArray;
 }
-
-var btn = document.getElementById('myButton');
-btn.addEventListener('click', function() {
-	myBtn(1);
-}, false);
